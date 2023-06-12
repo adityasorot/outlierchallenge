8e034bb9fbcdfb62d2575f190c36bb98cfe5b389
Aditya Singh <adityasorot@gmail.com>
2023-06-13 03:59:19 +0530

    feat: outlier challenge

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
+const { default: axios } = require('axios')
 const knex = require('./db')
-
+let gradeData;
 module.exports = {
   getHealth,
   getStudent,
@@ -7,7 +8,14 @@ module.exports = {
   getCourseGradesReport
 }
 
-async function getHealth (req, res, next) {
+const functionLoadup = async function() {
+  if(gradeData) return
+  gradeData = await axios.get('https://outlier-coding-test-data.onrender.com/grades.json')
+}
+
+functionLoadup()
+
+async function getHealth(req, res, next) {
   try {
     await knex('students').first()
     res.json({ success: true })
@@ -17,14 +25,65 @@ async function getHealth (req, res, next) {
   }
 }
 
-async function getStudent (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+async function getStudent(req, res, next) {
+  try {
+    const data = await knex('students').where('id', req.params.id)
+    if (data.length === 0) {
+      res.json({ success: false, error: "No data with the given id" })
+      return
+    }
+    res.json({ success: true, data })
+  } catch (e) {
+    console.log(e)
+    res.status(500).end()
+  }
 }
 
-async function getStudentGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+async function getStudentGradesReport(req, res, next) {
+  try {
+    await functionLoadup()
+    const studentGrade = gradeData.data.find(grades => grades.id === Number(req.params.id))
+    const data = await knex('students').where('id', req.params.id)
+    if (data.length === 0) {
+      res.json({ success: false, error: "No data with the given id" })
+      return
+    }
+    data[0].course = studentGrade.course
+    data[0].grade = studentGrade.grade
+    res.json({ success: true, data })
+  } catch (e) {
+    console.log(e)
+    res.status(500).end()
+  }
 }
 
-async function getCourseGradesReport (req, res, next) {
-  throw new Error('This method has not been implemented yet.')
+async function getCourseGradesReport(req, res, next) {
+  try {
+    await functionLoadup()
+    const data = {}
+    gradeData.data.forEach((grade) => {
+      if (data[grade.course] === undefined) {
+        data[grade.course] = {
+          max: 0,
+          min: 100,
+          count: 0,
+          sum: 0,
+          average: 0
+        }
+      }
+      if (data[grade.course].max < grade.grade) data[grade.course].max = grade.grade
+      if (data[grade.course].min > grade.grade) data[grade.course].min = grade.grade
+      data[grade.course].count += 1
+      data[grade.course].sum += grade.grade
+      data[grade.course].average = data[grade.course].sum / data[grade.course].count
+    })
+    Object.keys(data).forEach((course) => {
+      delete data[course].count
+      delete data[course].sum
+    })
+    res.json({ success: true, data })
+  } catch (e) {
+    console.log(e)
+    res.status(500).end()
+  }
 }
--- test.js
+++ test.js
@@ -20,6 +20,50 @@ tape('health', async function (t) {
   }
 })
 
+tape('student', async function (t) {
+  const url = `${endpoint}/student/1`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error('Error connecting to sqlite database; did you initialize it by running `npm run init-db`?')
+    }
+    t.equal(data.data[0].id, 1)
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
+})
+
+tape('studentGrades', async function (t) {
+  const url = `${endpoint}/student/1/grades`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error('Error connecting to sqlite database; did you initialize it by running `npm run init-db`?')
+    }
+    t.equal(data.data[0].id, 1)
+    t.equal(data.data[0].course, "Calculus")
+    t.equal(data.data[0].grade, 50)
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
+})
+
+tape('courseGrades', async function (t) {
+  const url = `${endpoint}/course/all/grades`
+  try {
+    const { data, response } = await jsonist.get(url)
+    if (response.statusCode !== 200) {
+      throw new Error('Error connecting to sqlite database; did you initialize it by running `npm run init-db`?')
+    }
+    t.equal(data.data.Calculus.max, 100)
+    t.end()
+  } catch (e) {
+    t.error(e)
+  }
+})
+
 tape('cleanup', function (t) {
   server.closeDB()
   server.close()
